dd253939d6102dd09948b67ae93659210037b9a2
Mark deVilliers <markdevilliers@gmail.com>
2014-02-27 20:59:35 +0000

    Updated batch file for new tagging layout.

--- build.bat
+++ build.bat
@@ -3,19 +3,19 @@ set version=%1
 git clone https://github.com/ckeditor/ckeditor-releases.git 
 
 cd ckeditor-releases
-git.exe checkout tags/%version%/basic
+git.exe checkout tags/basic/%version%
 cd ..
 
 .nuget\NuGet.exe pack ckeditor-basic.nuspec -version %version%
 
 cd ckeditor-releases
-git.exe checkout tags/%version%/standard
+git.exe checkout tags/standard/%version%
 cd ..
 
 .nuget\NuGet.exe pack ckeditor-standard.nuspec -version %version%
 
 cd ckeditor-releases
-git.exe checkout tags/%version%/full
+git.exe checkout tags/full/%version%
 cd ..
 .nuget\NuGet.exe pack ckeditor-full.nuspec -version %version%
 
